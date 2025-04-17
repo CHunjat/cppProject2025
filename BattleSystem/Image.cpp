5c60182b82757d6f14bcb9bb6e2d8b6c104c220e
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-04-17 17:49:59 +0900

    (2025-04-17) 전투 시스템 객체 구현

---
+++ BattleSystem/Image.cpp
@@ -0,0 +1,41 @@
+#include "Image.h"
+
+char SlimeIdle[IMAGEHEIGHT][IMAGEWIDTH + 1] =
+{
+	{"          "},
+	{"          "},
+	{"          "},
+	{" (****)   "},
+	{" (*  *)   "},
+	{" (****)   "},
+	{"          "},
+	{"          "},
+	{"          "},
+	{"          "}
+};
+char SlimeBattle[IMAGEHEIGHT][IMAGEWIDTH + 1] =
+{
+	{"  a  a    "},
+	{" a aa a   "},
+	{"   aa    "},
+	{" (****)   "},
+	{" (*  *)   "},
+	{" (****)   "},
+	{"          "},
+	{"          "},
+	{"          "},
+	{"          "}
+};
+char SlimeMove[IMAGEHEIGHT][IMAGEWIDTH + 1] =
+{
+	{"          "},
+	{"          "},
+	{"          "},
+	{" (m**m)   "},
+	{" (*mm*)   "},
+	{" (m**m)   "},
+	{"          "},
+	{"          "},
+	{"          "},
+	{"          "}
+};
---
+++ BattleSystem/Image.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Common.h"
+#define IMAGEHEIGHT 10
+#define IMAGEWIDTH  10
+
+//const int HEIGHT = 10;
+//const int WIDTH = 10;
+
+extern char SlimeIdle[IMAGEHEIGHT][IMAGEWIDTH + 1];
+extern char SlimeBattle[IMAGEHEIGHT][IMAGEWIDTH + 1];
+extern char SlimeMove[IMAGEHEIGHT][IMAGEWIDTH + 1];
