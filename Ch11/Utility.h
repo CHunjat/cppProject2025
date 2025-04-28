1452d412b0b3020de8c358af0bef2018635db588
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-04-28 17:47:00 +0900

    (2025-04-28) 달리기 게임

---
+++ Ch11/Utility.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <random>
+#include <Windows.h>
+
+// static : 클래스 객체 없이 범위로 함수를 호출할 수 있다.
+
+class RandomUtil
+{
+public:
+	static int GetRandomInt(int range);
+};
+
+class ConsoleUtil
+{
+public:
+	static void GotoXY(int x, int y);
+};
---
+++ Ch11/main.cpp
@@ -0,0 +1,84 @@
+// 클래스를 만드는 연습.
+
+#include "Base.h"
+
+void ClassDefaultInfo()
+{
+	Base* base = new Base();
+	delete base;
+
+	Base* derived = new Derived();
+	derived->Do();
+
+	delete derived;
+}
+
+/*
+*  달리기 게임 
+*   -----------------------
+* 	1
+* 	2
+* 	3
+* 	4
+*   ------------------------
+*/
+
+#include "Runner.h"
+int main()
+{
+	ClassDefaultInfo();
+	system("cls");
+	std::cout << "달리기 게임 코드" << std::endl;	// Game Title
+
+	Runner* runA = new Player();
+	Runner* runB = new Runner("B");
+	Runner* runC = new Runner();
+	Runner* runD = new Runner("D");
+	
+	string line = "===============================================================================";
+
+	int endline = line.length();
+
+	while (true)
+	{
+		Sleep(1000);   // 1초 마다 게임을 업데이트
+		system("cls"); // 화면을 지운다.
+
+		cout << line << endl;
+		runA->Run(); 
+		runB->Run();
+		runC->Run();
+		runD->Run();
+		cout << line << endl;
+				
+		if (runA->CheckEndLine(endline) 
+			|| runB->CheckEndLine(endline) 
+			|| runC->CheckEndLine(endline)
+			|| runD->CheckEndLine(endline))
+		{
+			cout << "경기 종료" << endl;
+			break;
+		}
+	}
+
+	if (runA->CheckEndLine(endline))
+	{
+		cout << "A가 1등을 했습니다." << endl;
+	}
+
+	if (runB->CheckEndLine(endline))
+	{
+		cout << "B가 1등을 했습니다." << endl;
+	}
+
+	if (runC->CheckEndLine(endline))
+	{
+		cout << "C가 1등을 했습니다." << endl;
+	}
+
+	if (runD->CheckEndLine(endline))
+	{
+		cout << "D가 1등을 했습니다." << endl;
+	}
+
+}
